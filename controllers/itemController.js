5dc4f49fe45e25e14bcb6789a34753a5a96b8104
Angel <skelliebunnie@gmail.com>
2021-08-15 17:04:05 -0700

    nvm, that doesn't help

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../models/index.js");
+const db = require("../models");
 
 const { Op } = require("sequelize");
 const jwt = require('jsonwebtoken');
