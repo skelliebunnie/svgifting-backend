e0e6bac92a78fa60fdbd4f7a6d1427a9e3f902d1
Angel <skelliebunnie@gmail.com>
2021-08-17 17:32:28 -0700

    changed root route return for easier testing

--- controllers/rootController.js
+++ controllers/rootController.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const db = require("../models");
+const Item = db.Item;
 
 router.get('/', function(req, res) {
-  return res.json({ message: 'success' })
+  Item.findAll({})
+    .then(results => res.json(results))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json(err)
+    })
+  
+  // return res.json({ message: 'success' })
 })
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 "use strict";
 
+require('dotenv').config()
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
@@ -8,15 +9,16 @@ var env = process.env.NODE_ENV || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
-if (config.use_env_variable) {
+if(config.use_env_variable) {
   var sequelize = new Sequelize(process.env.RDS_DBNAME, process.env.RDS_USERNAME, process.env.RDS_PASSWORD, {
   	host: process.env.RDS_HOSTNAME,
   	port: 3306,
   	dialect: 'mysql',
   	dialectModule: require('mysql2'),
   });
+  
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, config.password, config)
 }
 
 fs
